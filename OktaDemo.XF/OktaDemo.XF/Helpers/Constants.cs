74f098a2f64f12c0bbd3c46c299089ee7324563b
daniel.pollack@clubassist.com <daniel.pollack@clubassist.com>
2019-03-20 11:45:49 -0400

    checkin

---
+++ OktaDemo.XF/OktaDemo.XF/Helpers/Constants.cs
@@ -0,0 +1,20 @@
+﻿namespace OktaDemo.XF.Helpers
+{
+    public class Constants
+    {
+        public const string AuthStateKey = "authState";
+        public const string AuthServiceDiscoveryKey = "authServiceDiscovery";
+
+        public const string ClientId = "0oajq775tf4HM8h4v0h7";
+        public const string RedirectUri = "com.oktapreview.dev-900158:/callback";
+        public const string OrgUrl = "https://dev-900158.oktapreview.com";
+        public const string AuthorizationServerId = "default";
+
+        public static readonly string DiscoveryEndpoint =
+            $"{OrgUrl}/oauth2/{AuthorizationServerId}/.well-known/openid-configuration";
+
+
+        public static readonly string[] Scopes = new string[] {
+            "openid", "profile", "email", "offline_access" };
+    }
+}
